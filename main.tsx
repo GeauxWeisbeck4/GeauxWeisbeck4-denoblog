c6cb83cd5c18b7d2e9e180cca526cbdc729908b8
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-12-28 20:20:46 +0000

    Fixed issues with the post that caused deploy er

--- main.tsx
+++ main.tsx
@@ -1,10 +1,8 @@
 /** @jsx h */
 
 import blog, { ga, h, redirects } from "blog";
-import { bgBlack, white } from "https://deno.land/std@0.118.0/fmt/colors.ts";
 
 blog({
-  author: "Andrew Weisbeck",
   title: "GeauxWeisbeck4.dev Blog",
   description:
     "This blog is by Andrew Weisbeck and is the official GeauxWeisbeck4.dev blog. I hope you find it mentally stimulating, witty, and wholesome. All posts are of my own opinion, and do not represent the viewpoints of anybody else.",
@@ -13,14 +11,13 @@ blog({
   // footer: <footer>Your custom footer</footer>,
   avatar: "https://deno-avatar.deno.dev/avatar/29e6de.svg",
   avatarClass: "rounded-full",
+  author: "Andrew Weisbeck",
   links: [
     { title: "GitHub", url: "https://github.com/geauxweisbeck4" },
     { title: "Twitter", url: "https://twitter.com/geauxweisbeck4" },
     { title: "Homepage", url: "https://geauxweisbeck4.dev" },
     { title: "Mastodon", url: "https://fosstodon.org/@GeauxWeisbeck4" },
   ],
-  lang: "en",
-  dateStyle: "long",
   middlewares: [
     ga("UA-221895221-2"),
   ],
